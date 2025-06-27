d8000364f216b4754a8155a64e4c9808d22feddf
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-06-27 19:00:40 +0500

    Create Queryies for Product table

---
+++ Lootsy.ProductService.Api/Lootsy.ProductService.Application/Features/Queries/Products/GetAllProductsQuery.cs
@@ -0,0 +1,10 @@
+﻿using Lootsy.ProductService.Application.DTOs.Products;
+using Lootsy.ProductService.Application.Extensions;
+using Lootsy.ProductService.Application.QueryParamets;
+using MediatR;
+
+namespace Lootsy.ProductService.Application.Features.Queries.Products;
+
+public record GetAllProductsQuery(
+    ProductsQueryParameter ProductsQueryParameter
+    ) : IRequest<Result<List<GetAllProductsDto>>>;
---
+++ Lootsy.ProductService.Api/Lootsy.ProductService.Application/Features/Queries/Products/GetProductQuery.cs
@@ -0,0 +1,9 @@
+﻿using Lootsy.ProductService.Application.DTOs.Products;
+using Lootsy.ProductService.Application.Extensions;
+using MediatR;
+
+namespace Lootsy.ProductService.Application.Features.Queries.Products;
+
+public record GetProductQuery(
+    Guid ProductId
+    ) : IRequest<Result<ProductDto>>;

8e4941329d7d076cef5d5563049a7a2cdaea7fd5
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2025-06-27 18:59:02 +0500

    Add Dtos

---
+++ Lootsy.ProductService.Api/Lootsy.ProductService.Application/DTOs/Products/GetAllProductsDto.cs
@@ -0,0 +1,13 @@
+﻿using Lootsy.ProductService.Domain.Enums;
+
+namespace Lootsy.ProductService.Application.DTOs.Products;
+
+public record GetAllProductsDto(
+    Guid Id,
+    Guid SellerId,
+    string Name,
+    decimal Price,
+    string ImageUrl,
+    ProductStatus Status,
+    Guid CategoryId
+    );
